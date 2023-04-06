2b2a443c9aa638d2c29ff7c1dd43821d4db29d6d
D <dimmxp@gmail.com>
2023-04-06 16:04:22 +0300

    v2

---
+++ src/utils/api.js
@@ -0,0 +1,77 @@
+class Api {
+    #baseurl;
+    #headers;
+    constructor({ baseUrl, headers }) {
+        this.#baseurl = baseUrl;
+        this.#headers = headers;
+    }
+
+    #onResponse(res) {
+        return res.ok ? res.json() : res.json().then(err => Promise.reject(err))
+    }
+
+    getAllInfo() {
+        return Promise.all([this.getProductsList(), this.getUserInfo()])
+    }
+
+    getProductsList() {
+        return fetch(`${this.#baseurl}/products`, {
+            headers: this.#headers
+        })
+            .then(this.#onResponse)
+    }
+
+    getUserInfo() {
+        return fetch(`${this.#baseurl}/users/me`, {
+            headers: this.#headers
+        })
+            .then(this.#onResponse)
+    }
+
+    search(searchQuery) {
+        return fetch(`${this.#baseurl}/products/search?query=${searchQuery}`, {
+            headers: this.#headers
+        })
+            .then(this.#onResponse)
+    }
+
+    setUserInfo({ name, about }) {
+        return fetch(`${this.#baseurl}/users/me`, {
+            method: 'PATCH',
+            headers: this.#headers,
+            body: JSON.stringify({ name, about })
+        })
+            .then(this.#onResponse)
+    }
+
+    changeLikeProductStatus(productID, like) {
+        return fetch(`${this.#baseurl}/products/likes/${productID}`, {
+            method: like ? 'DELETE' : 'PUT',
+            headers: this.#headers,
+        })
+            .then(this.#onResponse)
+    }
+
+    getProductById(idProduct) {
+        return fetch(`${this.#baseurl}/products/${idProduct}`, {
+            headers: this.#headers
+        })
+            .then(this.#onResponse)
+    }
+
+    getInfoProduct(idProduct) {
+        return Promise.all([this.getProductById(idProduct), this.getUserInfo()])
+    }
+}
+
+
+const api = new Api({
+    baseUrl: 'https://api.react-learning.ru',
+    headers: {
+        'content-type': 'application/json',
+        authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEwN2UwOWFhMzk3MTIxODM4ZjI5MWMiLCJncm91cCI6Imdyb3VwLTExIiwiaWF0IjoxNjc4ODAyNDQ5LCJleHAiOjE3MTAzMzg0NDl9.dvWkHpISMy3ox6hb58sbRlPZ61ceeAPeqshkZQwxgGQ'
+    }
+})
+
+
+export default api;
